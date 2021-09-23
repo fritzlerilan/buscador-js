c5a263db6311150beb8873d51d5112bc31709802
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-22 22:58:54 -0300

    Make Script to fill the Year Option Panel

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 // Variables
-const div_resultado = document.querySelector('#resultado');
+const divResultado = document.querySelector('#resultado');
+const selectYear = document.querySelector('#year');
 
+const maxYear = new Date().getFullYear();
+const minYear = maxYear - 10;
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos();
+    mostrarAutos(); //Muestra los autos al cargar
+    llenarSelect(); //Llena los campos de seleccion segun la informacion en la DB
+
 })
 
 // Funciones
@@ -18,6 +23,15 @@ function mostrarAutos() {
             ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmision: ${transmision} - Precio: $ ${precio} - Color: ${color}
         `;
         // Se añade al DOM
-        div_resultado.appendChild(autoHTML);
+        divResultado.appendChild(autoHTML);
     });
+}
+
+function llenarSelect() {
+    for(let i = maxYear; i >= minYear; i--) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option); // Agrega las opciones de año al select
+    }
 }
